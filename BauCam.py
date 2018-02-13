cb6a5943f69f374afaa8a331287e3befed4b81cc
Wene <wene83@gmx.ch>
2018-02-13 22:04:17 +0100

    make timeout independent from photo duration

--- BauCam.py
+++ BauCam.py
@@ -3,14 +3,21 @@
 import os
 import subprocess
 from time import sleep
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def take_photos():
     os.chdir('pictures/capture')
 
+    now = datetime.now()
+    last_time = now - timedelta(seconds=28)
     for i in range(10):
-        pic_name = datetime.now().strftime('img_%Y-%m-%d_%H-%M-%S')
+        # calculate cycle time
+        while now < last_time + timedelta(seconds=30):
+            now = datetime.now()
+            print(now.isoformat(), last_time.isoformat())
+            sleep(1)
+        last_time = now
 
         # clean up the directory
         files = os.listdir()
@@ -18,7 +25,8 @@ def take_photos():
             os.remove(file_name)
 
         try:
-            out_bytes = subprocess.run(['gphoto2', '--capture-image-and-download'], timeout=15, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout
+            out_bytes = subprocess.run(['gphoto2', '--capture-image-and-download'], timeout=15,
+                                       stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout
             output = str(out_bytes, 'utf-8')
             print('Ausgabe:')
             print('---')
@@ -26,12 +34,14 @@ def take_photos():
             files = os.listdir()
             for file_name in files:
                 base, ext = os.path.splitext(file_name)
+                pic_name = now.strftime('img_%Y-%m-%d_%H-%M-%S')
                 os.rename(file_name, os.path.join('..', pic_name + ext))
+        except subprocess.TimeoutExpired as e:
+            print('Timeout beim Versuch zu fotografieren...')
         except Exception as e:
             print('Unbekannter Ausnahmefehler:')
             print(type(e))
             print(e)
-        sleep(10)
 
 
 if __name__ == '__main__':
