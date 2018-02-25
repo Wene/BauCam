fd81fe08b78c3e49667487aa0278c445d16f738e
Wene <wene83@gmx.ch>
2018-02-25 17:51:25 +0100

    read EXIF data and use RasPi GPIO pin to reboot camera

--- BauCam.py
+++ BauCam.py
@@ -7,9 +7,11 @@ import configparser
 import sqlite3
 from time import sleep
 from datetime import datetime, timedelta
+import gpiozero
+import sys
 
 
-def take_photo(capture_path, local_path):
+def take_photo(capture_path, local_path, pic_name):
     os.chdir(capture_path)
 
     # clean up the directory
@@ -24,12 +26,17 @@ def take_photo(capture_path, local_path):
         print('Ausgabe:')
         print('---')
         print(str(output))
+        ret = ['']
         files = os.listdir()
         for file_name in files:
             base, ext = os.path.splitext(file_name)
-            pic_name = now.strftime('img_%Y-%m-%d_%H-%M-%S')
-            os.rename(file_name, os.path.join(local_path, pic_name + ext))
-        return files
+            new_name = pic_name + ext
+            os.rename(file_name, os.path.join(local_path, new_name))
+            if '.jpg' == ext.lower() or '.jpeg' == ext.lower():
+                ret[0] = new_name
+            else:
+                ret.append(new_name)
+        return ret
     except subprocess.TimeoutExpired as e:
         print('Timeout beim Versuch zu fotografieren...')
     except Exception as e:
@@ -64,19 +71,48 @@ if __name__ == '__main__':
         os.makedirs(remote_path)
     interval = general_conf.getint('interval seconds')
 
+    # connect relais module and restart camera
+    camera = gpiozero.DigitalOutputDevice(pin=3, active_high=False)
+    camera.off()
+    sleep(5)
+    camera.on()
+
     # check if the clock is possibly set correctly (this script should run on a Raspberry Pi without RTC)
     if datetime.now() > datetime(2018, 2, 1, 0, 0):
         now = datetime.now()
         last_time = now - timedelta(seconds=interval - 2)
+        no_photo_taken = 0
         for i in range(10):
             # calculate cycle time
             while now < last_time + timedelta(seconds=interval):
                 now = datetime.now()
-                print(now.isoformat(), last_time.isoformat())
+                diff = now - last_time
+                print(diff.total_seconds())
                 sleep(1)
             last_time = now
-            files = take_photo(capture_path, local_path)
-            for name in files:
-                print(name)
+            pic_name = now.strftime('img_%Y-%m-%d_%H-%M-%S')
+            files = take_photo(capture_path, local_path, pic_name)
+            if files[0] != '':
+                no_photo_taken = 0
+                exif_path = os.path.join(local_path, files[0])
+                with open(exif_path, 'rb') as f:
+                    exif_tags = exifread.process_file(f)
+                print('Dateiname', exif_path)
+                print('Verschlusszeit', exif_tags['EXIF ExposureTime'])
+                print('ISO', exif_tags['EXIF ISOSpeedRatings'])
+                print('Blende', exif_tags['EXIF FNumber'])
+                print('Aufnahmedatum', exif_tags['EXIF DateTimeOriginal'])
+            else:
+                no_photo_taken += 1
+            # TODO: take actual rebooting measures
+            if no_photo_taken > 3:
+                print('rebooting everything')
+                sys.exit('reboot required')
+            if no_photo_taken > 2:
+                print('rebooting camera')
+                camera.off()
+                sleep(10)
+                camera.on()
+
     else:
         print('Systemdatum falsch')
