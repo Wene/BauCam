de50a9948aa7f01a4abfcf1b131a1ef4628ba65f
Wene <wene83@gmx.ch>
2018-11-04 13:23:25 +0100

    raw structure defined

--- cleanup.py
+++ cleanup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
+import configparser
+import os
+import sqlite3
+import sys, errno
 
 parser = argparse.ArgumentParser(description='cleanup unwanted pictures from local and remote storage and the database')
 parser.add_argument('-d', '--delete', help='actually perform the delete action', action='store_true')
@@ -9,4 +13,26 @@ args = parser.parse_args()
 
 print('---   ---   ---')
 print(args)
-print(args.delete)
+print('---   ---   ---')
+
+conf = configparser.ConfigParser()
+if os.path.isfile('BauCam.conf'):
+    conf.read('BauCam.conf')
+    general_conf = conf['general']
+    database_path = os.path.expanduser(general_conf.get('database path'))
+else:
+    print("BauCam.conf not found!")
+    sys.exit(errno.EACCES)
+
+if os.path.isfile(database_path):
+    conn = sqlite3.connect(database_path)
+    cur = conn.cursor()
+else:
+    print("Database not found!")
+    sys.exit(errno.EACCES)
+
+if args.list:
+    print("List is active")
+
+if args.delete:
+    print("Delete is active")
