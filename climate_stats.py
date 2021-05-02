d0bdb9b4ba451a3a8ab0542e9b1cf6c58f163ea1
Wene <wene83@gmx.ch>
2021-05-02 17:17:06 +0200

    format x axis as date

--- climate_stats.py
+++ climate_stats.py
@@ -4,7 +4,8 @@ import sqlite3
 import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
-
+from matplotlib.dates import date2num, DateFormatter
+from datetime import datetime
 
 class DB:
     def __init__(self):
@@ -18,14 +19,24 @@ class DB:
         cur.execute('SELECT "raspi_time", "humidity", "temperature" from climate '
                     'WHERE "humidity" NOT NULL AND "temperature" NOT NULL ORDER BY "id";')
         result = cur.fetchall()
-        return result
+        dates = []
+        values = []
+        for entry in result:
+            date_str = entry[0]
+            date = datetime.fromisoformat(date_str)
+            date_num = date2num(date)
+            dates.append(date_num)
+            values.append(entry[1:])
+        return dates, values
 
 
 if '__main__' == __name__:
     db = DB()
-    all_entries = db.get_climate()
+    dates, values = db.get_climate()
     del db
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    ax.plot(all_entries)
+    fig, ax = plt.subplots()
+    ax.plot(dates, values)
+    ax.set(xlabel='Date', ylabel='Value', title='Climate data')
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d %H:%M'))
+    plt.setp(ax.get_xticklabels(), rotation=45)
     plt.show()
